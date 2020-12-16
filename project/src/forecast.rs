aef2685524a40150cc6fb2eace9225b3b843c99f
Markus Reiter <me@reitermark.us>
2020-12-16 22:50:53 +0100

    Add Dockerfile for Rust functions.

--- project/src/forecast.rs
+++ project/src/forecast.rs
@@ -1,2 +1,13 @@
-fn main() {
+use std::env;
+use std::io;
+
+use anyhow::Result;
+use serde_json::Value;
+
+fn main() -> Result<()> {
+  let params: Value = serde_json::from_str(&env::args().nth(1).unwrap())?;
+
+  let result = params;
+  serde_json::to_writer(io::stdout(), &result)?;
+  Ok(())
 }
