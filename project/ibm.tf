edc4514404141896fe35f3b7f9d801f5d801ec1d
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2020-12-24 00:22:15 +0100

    Add `fetch_prices_js` to terraform.

--- project/ibm.tf
+++ project/ibm.tf
@@ -1,9 +1,10 @@
 variable "ibmcloud_api_key" {}
+variable "ibmcloud_washington_namespace" {}
 
 terraform {
   required_providers {
     ibm = {
-      source = "IBM-Cloud/ibm"
+      source  = "IBM-Cloud/ibm"
       version = "~> 1.12.0"
     }
   }
@@ -14,12 +15,12 @@ provider "ibm" {
   region           = "us-east"
 }
 
-resource "ibm_function_action" "fetch_prices" {
-  name      = "fetch_prices"
-  namespace = "washington"
+resource "ibm_function_action" "fetch_prices_rs" {
+  name      = "fetch_prices_rs"
+  namespace = var.ibmcloud_washington_namespace
 
   exec {
-    kind  = "blackbox"
+    kind  = "Blackbox"
     image = "openwhisk/dockerskeleton"
     code  = filebase64("target/fetch_prices.zip")
   }
@@ -29,3 +30,19 @@ resource "ibm_function_action" "fetch_prices" {
     memory  = 128
   }
 }
+
+resource "ibm_function_action" "fetch_prices_js" {
+  name      = "fetch_prices_js"
+  namespace = var.ibmcloud_washington_namespace
+
+  exec {
+    kind = "nodejs:12"
+    code = file("dist/fetch_prices.bundle.js")
+  }
+
+  limits {
+    timeout = 10000
+    memory  = 128
+  }
+}
+
