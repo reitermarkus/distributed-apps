ec0d78e7e5ed488eeea5c728e535e0255dd27c6c
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2020-12-29 00:27:04 +0100

    Get share from parameter.

--- project/src/fetch_prices.ts
+++ project/src/fetch_prices.ts
@@ -1,7 +1,18 @@
 const alpha = require('alphavantage')({ key: process.env.ALPHA_KEY })
 
-async function main(params: object) {
-  return await alpha.data.daily('msft')
+interface Params {
+  share: string;
+}
+
+function assert(value: any): asserts value {
+  if (!value) {
+    throw new Error('A share must be specified')
+  }
+}
+
+async function main(params: Params) {
+  assert(params?.share)
+  return await alpha.data.daily(params?.share)
 }
 
 exports.main = main
