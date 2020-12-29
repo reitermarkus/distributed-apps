712034a94fe99414684a8fce7f2093a13acf0e8f
Markus Reiter <me@reitermark.us>
2020-12-29 05:14:53 +0100

    Fix build script.

--- project/build_rust_function.sh
+++ project/build_rust_function.sh
@@ -16,7 +16,10 @@ rust-musl-builder() {
 
 rust-musl-builder sudo chown -R rust:rust /home/rust/.cargo/git /home/rust/.cargo/registry 1>&2
 rust-musl-builder cargo build --bin "${name}" --release 1>&2
-cp -f "target/x86_64-unknown-linux-musl/release/${name}" exec 1>&2
-zip -9 -mj "${archive}" exec 1>&2
+
+mkdir -p "${archive}.d"
+cp -f "target/x86_64-unknown-linux-musl/release/${name}" "${archive}.d/exec" 1>&2
+zip -9 -FS -m -j "${archive}" "${archive}.d/exec" 1>&2
+rm -rf "${archive}.d"
 
 echo "{\"filename\":\"${archive}\",\"id\":\"$(date +%s)\"}"
