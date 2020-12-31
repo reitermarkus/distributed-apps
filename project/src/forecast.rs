4a54a8b15f174f1d6ed854f9eea857cdf466163f
Markus Reiter <me@reitermark.us>
2020-12-31 23:31:39 +0100

    Generate forecast predictions.

--- project/src/forecast.rs
+++ project/src/forecast.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 use std::env;
 use std::io;
 use std::time::Duration;
@@ -9,6 +9,7 @@ use dotenv_codegen::dotenv;
 use rusoto_credential::StaticProvider;
 use rusoto_core::{Region, HttpClient, RusotoError};
 use rusoto_forecast::*;
+use rusoto_forecastquery::{ForecastQueryClient, ForecastQuery, QueryForecastRequest, DataPoint};
 use s3::Bucket;
 use s3::creds::Credentials;
 use serde::{Serialize, Deserialize};
@@ -34,20 +35,23 @@ struct Input {
 #[derive(Debug, Serialize)]
 struct Output {
   symbol: String,
+  timestamp: String,
+  price_high: f64,
 }
 
 #[derive(Debug, Serialize)]
 struct Row {
   item_id: String,
   timestamp: String,
-  price_open: f32,
-  price_high: f32,
-  price_low: f32,
-  price_close: f32,
-  price_adjusted_close: f32,
-  volume: usize,
-  dividend_amount: f32,
-  split_coefficient: f32,
+  target_value: f32,
+  // price_open: f32,
+  // price_high: f32,
+  // price_low: f32,
+  // price_close: f32,
+  // price_adjusted_close: f32,
+  // volume: usize,
+  // dividend_amount: f32,
+  // split_coefficient: f32,
 }
 
 macro_rules! try_response {
@@ -56,6 +60,23 @@ macro_rules! try_response {
     match response_code {
       200..=299 => response,
       _ => return Err(Error::msg(String::from_utf8(response)?)),
+    }
+  }}
+}
+
+macro_rules! wait_until_active {
+  ($command:expr) => {{
+    loop {
+      let result = $command.await?;
+
+      if let Some(status) = &result.status {
+        eprintln!("Status: {}", status);
+        if status == "ACTIVE" || status == "CREATE_FAILED" {
+          break result;
+        }
+      }
+
+      sleep(Duration::from_secs(10)).await;
     }
   }}
 }
@@ -101,21 +122,22 @@ async fn forecast(params: Value) -> Result<Output> {
 
   let bucket = ibm::bucket().await?;
   let response = try_response!(bucket.get_object(object_key).await?);
-  let response: HashMap<String, TimeSeriesDailyAdjusted> = serde_json::from_slice(&response)?;
+  let response: BTreeMap<String, TimeSeriesDailyAdjusted> = serde_json::from_slice(&response)?;
 
   let mut csv = Writer::from_writer(vec![]);
-  for (k, v) in response {
+  for (k, v) in response.into_iter().rev().take(7) {
     csv.serialize(Row {
       item_id: symbol.clone(),
       timestamp: k,
-      price_open: v.open,
-      price_high: v.high,
-      price_low: v.low,
-      price_close: v.close,
-      price_adjusted_close: v.adjusted_close,
-      volume: v.volume,
-      dividend_amount: v.dividend_amount,
-      split_coefficient: v.split_coefficient,
+      target_value: v.high,
+      // price_open: v.open,
+      // price_high: v.high,
+      // price_low: v.low,
+      // price_close: v.close,
+      // price_adjusted_close: v.adjusted_close,
+      // volume: v.volume,
+      // dividend_amount: v.dividend_amount,
+      // split_coefficient: v.split_coefficient,
     })?;
   }
 
@@ -129,32 +151,33 @@ async fn forecast(params: Value) -> Result<Output> {
   dbg!(&csv_object);
 
   let credential_provider = StaticProvider::new(AWS_ACCESS_KEY_ID.into(), AWS_SECRET_KEY.into(), Some(AWS_SESSION_TOKEN.into()), None);
-  let forecast_client = ForecastClient::new_with(HttpClient::new().unwrap(), credential_provider, Region::UsEast1);
+  let forecast_client = ForecastClient::new_with(HttpClient::new().unwrap(), credential_provider.clone(), Region::UsEast1);
+  let forecast_query_client = ForecastQueryClient::new_with(HttpClient::new().unwrap(), credential_provider, Region::UsEast1);
 
   let dataset_import_jobs = forecast_client.list_dataset_import_jobs(Default::default()).await?;
   dbg!(&dataset_import_jobs);
 
-  if let Some(dataset_import_jobs) = dataset_import_jobs.dataset_import_jobs {
-    for dataset_import_job in dataset_import_jobs {
-      if let Some(dataset_import_job_arn) = dataset_import_job.dataset_import_job_arn {
-        delete_dataset_import_job(&forecast_client, dataset_import_job_arn).await?;
-      }
-    }
-  }
+  // if let Some(dataset_import_jobs) = dataset_import_jobs.dataset_import_jobs {
+  //   for dataset_import_job in dataset_import_jobs {
+  //     if let Some(dataset_import_job_arn) = dataset_import_job.dataset_import_job_arn {
+  //       delete_dataset_import_job(&forecast_client, dataset_import_job_arn).await?;
+  //     }
+  //   }
+  // }
 
-  let dataset_name = format!("{}_price_forecast", symbol);
+  let dataset_name = format!("{}_dataset", symbol);
 
   let datasets = forecast_client.list_datasets(Default::default()).await?;
   dbg!(&datasets);
 
-  if let Some(datasets) = datasets.datasets {
-    for dataset in datasets {
-      if let Some(dataset_arn) = dataset.dataset_arn {
-        forecast_client.delete_dataset(DeleteDatasetRequest { dataset_arn }).await?;
-        sleep(Duration::from_secs(10)).await;
-      }
-    }
-  }
+  // if let Some(datasets) = datasets.datasets {
+  //   for dataset in datasets {
+  //     if let Some(dataset_arn) = dataset.dataset_arn {
+  //       forecast_client.delete_dataset(DeleteDatasetRequest { dataset_arn }).await?;
+  //       sleep(Duration::from_secs(10)).await;
+  //     }
+  //   }
+  // }
 
   let dataset_group_arn = "arn:aws:forecast:us-east-1:860352936990:dataset-group/stock_forecast_group";
 
@@ -163,27 +186,30 @@ async fn forecast(params: Value) -> Result<Output> {
   }).await?;
   dbg!(&group);
 
-  let dataset_arn = group.dataset_arns.unwrap_or_else(|| vec![]).into_iter().find(|dataset_arn| {
-    dataset_arn.rsplitn(2, "/").next().unwrap() == &dataset_name
+  let dataset_arns: Vec<String> = group.dataset_arns.unwrap_or_else(|| vec![]);
+  dbg!(&dataset_arns);
+  let dataset_arn: Option<String> = dataset_arns.into_iter().find(|dataset_arn| {
+    dataset_arn.rsplitn(2, "/").next().as_deref() == Some(&dataset_name)
   });
+  dbg!(&dataset_arn);
+
+  let schema: Schema = serde_json::from_str(include_str!("amazon_forecast_target_schema.json")).unwrap();
 
   let dataset_arn = if let Some(dataset_arn) = dataset_arn {
     dataset_arn
   } else {
     eprintln!("Creating dataset {}.", dataset_name);
 
-    let schema: Schema = serde_json::from_str(include_str!("amazon_forecast_schema.json")).unwrap();
     let request = CreateDatasetRequest {
       data_frequency: Some("D".into()),
       dataset_name,
-      dataset_type: "RELATED_TIME_SERIES".into(),
+      dataset_type: "TARGET_TIME_SERIES".into(),
       domain: "CUSTOM".into(),
       encryption_config: None,
-      schema,
+      schema: schema.clone(),
       tags: None,
     };
     let dataset = forecast_client.create_dataset(request).await?;
-    dbg!(&dataset);
 
     forecast_client.update_dataset_group(UpdateDatasetGroupRequest {
       dataset_group_arn: dataset_group_arn.into(),
@@ -193,7 +219,7 @@ async fn forecast(params: Value) -> Result<Output> {
     dataset.dataset_arn.unwrap()
   };
 
-  dbg!(&dataset_arn);
+  eprintln!("Dataset ARN: {}", dataset_arn);
 
   let import_dataset = || async {
     let data_source = DataSource {
@@ -203,49 +229,115 @@ async fn forecast(params: Value) -> Result<Output> {
         role_arn: AWS_FORECAST_ROLE.into(),
       }
     };
+
+    let dataset_import_job_name = format!("{}_dataset_import", symbol);
     let result = forecast_client.create_dataset_import_job(CreateDatasetImportJobRequest {
       data_source,
       dataset_arn: dataset_arn.clone(),
-      dataset_import_job_name: format!("import_{}", symbol),
+      dataset_import_job_name: dataset_import_job_name.clone(),
       tags: None,
       timestamp_format: Some("yyyy-MM-dd".into()),
-    }).await?;
+    }).await;
 
-    let dataset_import_job_arn = result.dataset_import_job_arn.unwrap();
-    dbg!(&dataset_import_job_arn);
-    let request = DescribeDatasetImportJobRequest { dataset_import_job_arn: dataset_import_job_arn.clone() };
-
-    let import_job = loop {
-      let import_job = forecast_client.describe_dataset_import_job(request.clone()).await?;
-
-      if let Some(status) = &import_job.status {
-        if status == "ACTIVE" || status == "CREATE_FAILED" {
-          break import_job;
-        }
-      }
-      dbg!(&import_job);
-
-      sleep(Duration::from_secs(10)).await;
+    let dataset_import_job_arn = match result {
+      Ok(result) => result.dataset_import_job_arn.unwrap(),
+      Err(RusotoError::Service(CreateDatasetImportJobError::ResourceAlreadyExists(msg))) => {
+        msg.rsplitn(2, "arn: ").next().unwrap().to_owned()
+      },
+      Err(err) => return Err(err.into()),
     };
-    dbg!(&import_job);
+
+    eprintln!("Import job '{}' started.", dataset_import_job_arn);
+
+    let import_job = wait_until_active!(forecast_client.describe_dataset_import_job(DescribeDatasetImportJobRequest {
+      dataset_import_job_arn: dataset_import_job_arn.clone(),
+    }));
 
     let creation_time = import_job.creation_time.unwrap_or(0.0);
     let last_modification_date = import_job.last_modification_time.unwrap_or(0.0);
 
     eprintln!("{} import job took {} seconds.", symbol, (last_modification_date - creation_time).round());
 
-    delete_dataset_import_job(&forecast_client, dataset_import_job_arn).await?;
+    let predictor_name = format!("{}_predictor", symbol);
+    let result = forecast_client.create_predictor(CreatePredictorRequest {
+      algorithm_arn: Some("arn:aws:forecast:::algorithm/CNN-QR".into()),
+      predictor_name,
+      featurization_config: FeaturizationConfig {
+        // forecast_dimensions: Some(vec!["target_value".into(), "price_high".into(), "price_low".into(), "price_close".into(), "price_adjusted_close".into()]),
+        forecast_frequency: "D".into(),
+        ..Default::default()
+      },
+      forecast_horizon: 1,
+      input_data_config: InputDataConfig {
+        dataset_group_arn: dataset_group_arn.into(),
+        ..Default::default()
+      },
+      ..Default::default()
+    }).await;
 
-    Ok::<(), Error>(())
+    let predictor_arn = match result {
+      Ok(result) => result.predictor_arn.unwrap(),
+      Err(RusotoError::Service(CreatePredictorError::ResourceAlreadyExists(msg))) => {
+        msg.rsplitn(2, "arn: ").next().unwrap().to_owned()
+      },
+      Err(err) => return Err(err.into()),
+    };
+    dbg!(&predictor_arn);
+
+    let predictor = wait_until_active!(forecast_client.describe_predictor(DescribePredictorRequest {
+      predictor_arn: predictor_arn.clone(),
+    }));
+    dbg!(&predictor);
+
+    let forecast_name = format!("{}_forecast", symbol);
+    let result = forecast_client.create_forecast(CreateForecastRequest {
+      forecast_name,
+      predictor_arn,
+      ..Default::default()
+    }).await;
+    let forecast_arn = match result {
+      Ok(result) => result.forecast_arn.unwrap(),
+      Err(RusotoError::Service(CreateForecastError::ResourceAlreadyExists(msg))) => {
+        msg.rsplitn(2, "arn: ").next().unwrap().to_owned()
+      },
+      Err(err) => return Err(err.into()),
+    };
+    dbg!(&forecast_arn);
+
+    let forecast = wait_until_active!(forecast_client.describe_forecast(DescribeForecastRequest {
+      forecast_arn: forecast_arn.clone(),
+    }));
+    dbg!(&forecast);
+
+    let mut filters = HashMap::<String, String>::new();
+    filters.insert("item_id".into(), symbol.clone());
+    let result = forecast_query_client.query_forecast(QueryForecastRequest {
+      forecast_arn,
+      filters,
+      ..Default::default()
+    }).await?;
+
+    let predictions = result.forecast.unwrap().predictions.unwrap();
+
+    // delete_dataset_import_job(&forecast_client, dataset_import_job_arn).await?;
+
+    Ok::<HashMap<String, Vec<DataPoint>>, Error>(predictions)
   };
 
   let import_result = import_dataset().await;
 
-  forecast_client.delete_dataset(DeleteDatasetRequest { dataset_arn }).await?;
+  // forecast_client.delete_dataset(DeleteDatasetRequest { dataset_arn }).await?;
 
-  import_result?;
+  let mut predictions = import_result?;
+  dbg!(&predictions);
 
-  Ok(Output { symbol })
+  let prediction = predictions.remove("p50").unwrap().pop().unwrap();
+
+  Ok(Output {
+    symbol,
+    timestamp: prediction.timestamp.unwrap(),
+    price_high: prediction.value.unwrap(),
+  })
 }
 
 #[tokio::main]
