d600d4ccd83d7967792e7964c82d8a2c95d31af4
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2021-01-05 03:18:49 +0100

    Fix forecast S3 upload.

--- project/src/forecast.ts
+++ project/src/forecast.ts
@@ -3,12 +3,6 @@ import { createObjectCsvWriter as createCsvWriter } from 'csv-writer'
 import fetch from 'node-fetch'
 const s3 = require('@auth0/s3')
 
-// const {
-  // S3Client,
-  // PutObjectCommand,
-  // CreateBucketCommand
-// } = require("@aws-sdk/client-s3")
-
 const getIbmBucketContent = async (symbol: string) : Promise<void> => {
   const endpoint = process.env.IBM_OBJECT_STORAGE_ENDPOINT_URL
   const bucket = process.env.IBM_OBJECT_STORAGE_BUCKET_NAME
@@ -31,7 +25,7 @@ const symbolDataToCsv = async (symbol: string, bucket_content: any) : Promise<vo
   const csvWriter = createCsvWriter({
     header: [
       {id: 'symbol', title: 'item_id'},
-      {id: 'timestamp', title: 'high'},
+      {id: 'timestamp', title: 'timestamp'},
       {id: 'high', title: 'target_value'},
     ],
     path: `${symbol}.csv`
@@ -41,35 +35,6 @@ const symbolDataToCsv = async (symbol: string, bucket_content: any) : Promise<vo
 }
 
 const awsBucketManagement = async (symbol: string) => {
-  // const REGION = 'us-east-1'
-//
-  // const bucketName = process.env.AWS_FORECAST_BUCKET
-  // const bucketParams = { Bucket: bucketName };
-//
-  // const keyName = `${symbol}.csv`;
-  // const objectParams = { Bucket: bucketName, Key: keyName, Body: 'item_id,high,target_value\nmsft,2020-12-28,226.0300' }
-//
-  // console.log(bucketParams)
-//
-  // const s3 = new S3Client({
-    // region: REGION,
-  // });
-//
-  // try {
-    // const data = await s3.send(new CreateBucketCommand(bucketParams));
-    // console.log("Success. Bucket created.");
-  // } catch (err) {
-    // console.log("Error", err);
-  // }
-  // try {
-    // const results = await s3.send(new PutObjectCommand(objectParams));
-    // console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
-  // } catch (err) {
-    // console.log("Error", err);
-  // }
-
-  console.log(process.env.AWS_ACCESS_KEY_ID)
-
   var client = s3.createClient({
     s3Options: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -84,8 +49,6 @@ const awsBucketManagement = async (symbol: string) => {
     s3Params: {
       Bucket: process.env.AWS_FORECAST_BUCKET,
       Key: `${symbol}.csv`,
-      // other options supported by putObject, except Body and ContentLength.
-      // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
     },
   };
 
