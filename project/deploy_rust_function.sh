ab97d57404809bcddf3aafd10d6b1e6654a29272
Markus Reiter <me@reitermark.us>
2021-01-06 02:34:32 +0100

    Use `update` instead of `delete` and `create`.

--- project/deploy_rust_function.sh
+++ project/deploy_rust_function.sh
@@ -8,5 +8,4 @@ archive="${3}"
 
 ibmcloud login -r us-east -g Default --apikey @ibmcloud_api_key.txt
 ibmcloud fn namespace target "${namespace}"
-ibmcloud fn action delete "${name}" || true
-ibmcloud fn action create "${name}" --timeout 10000 --memory 128 --web true --native "${archive}"
+ibmcloud fn action update "${name}" --timeout 10000 --memory 128 --web true --native "${archive}"
