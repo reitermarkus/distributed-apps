2746fe6c244b7187cc29f584ad23fcd93b8aea31
Markus Reiter <me@reitermark.us>
2021-01-18 22:46:44 +0100

    Convert AFCL to graph.

---
+++ homework-07/scheduler/src/afcl.rs
@@ -0,0 +1,178 @@
+use std::collections::HashMap;
+
+use petgraph::{Graph, graph::NodeIndex};
+use serde_derive::{Deserialize, Serialize};
+use serde_yaml::Value;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FunctionChoreography {
+  name: String,
+  data_ins: Option<Vec<DataIO>>,
+  data_outs: Option<Vec<DataIO>>,
+  workflow_body: Vec<Block>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum Block {
+  #[serde(rename_all = "camelCase")]
+  Function {
+    name: String,
+    #[serde(rename = "type")]
+    function_type: String,
+    data_ins: Option<Vec<DataIO>>,
+    data_outs: Option<Vec<DataIO>>,
+    constraints: Option<Vec<Constraint>>,
+  },
+  #[serde(rename_all = "camelCase")]
+  ParallelFor {
+    name: String,
+    data_ins: Option<Vec<DataIO>>,
+    data_outs: Option<Vec<DataIO>>,
+    loopCounter: LoopCounter,
+    loopBody: Vec<Block>,
+  },
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct LoopCounter {
+  from: String,
+  to: String,
+  step: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct DataIO {
+  name: String,
+  #[serde(rename = "type")]
+  data_type: String,
+  source: Option<String>,
+  passing: Option<bool>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Constraint {
+  name: String,
+  value: String,
+}
+
+trait AddToGraph {
+  fn add_to_graph<'a>(&'a self, graph: &mut Graph<&'a str, ()>, functions: &mut HashMap<&'a str, NodeIndex>);
+}
+
+impl AddToGraph for FunctionChoreography {
+  fn add_to_graph<'a>(&'a self, graph: &mut Graph<&'a str, ()>, functions: &mut HashMap<&'a str, NodeIndex>) {
+    let index = function_index(graph, functions, &self.name);
+
+    for block in &self.workflow_body {
+      block.add_to_graph(graph, functions);
+    }
+  }
+}
+
+fn function_index<'a>(graph: &mut Graph<&'a str, ()>, functions: &mut HashMap<&'a str, NodeIndex>, name: &'a str) -> NodeIndex {
+  if let Some(i) = functions.get(name) {
+    *i
+  } else {
+    let i = graph.add_node(name);
+    functions.insert(name, i);
+    i
+  }
+}
+
+fn source_index<'a>(graph: &mut Graph<&'a str, ()>, functions: &mut HashMap<&'a str, NodeIndex>, source: Option<&'a str>) -> Option<NodeIndex> {
+  if let Some(source) = &source {
+    if source.contains("/") {
+      if let Some(source) = source.splitn(2, "/").nth(0) {
+        return Some(function_index(graph, functions, source))
+      }
+    }
+  }
+
+  None
+}
+
+impl AddToGraph for Block {
+  fn add_to_graph<'a>(&'a self, graph: &mut Graph<&'a str, ()>, functions: &mut HashMap<&'a str, NodeIndex>) {
+    match self {
+      Self::Function { name, data_ins, data_outs, .. } => {
+        let index = function_index(graph, functions, name);
+
+        if let Some(data_ins) = data_ins {
+          for data_in in data_ins {
+            if let Some(source_index) = source_index(graph, functions, data_in.source.as_deref()) {
+              graph.add_edge(index, source_index, ());
+            }
+          }
+        }
+
+        if let Some(data_outs) = data_outs {
+          for data_out in data_outs {
+            if let Some(source_index) = source_index(graph, functions, data_out.source.as_deref()) {
+              graph.add_edge(index, source_index, ());
+            }
+          }
+        }
+      },
+      Self::ParallelFor { name, loopCounter, loopBody, data_ins, data_outs, .. } => {
+        let index = function_index(graph, functions, name);
+
+        if let Some(source_index) = source_index(graph, functions, Some(&loopCounter.from)) {
+          graph.add_edge(index, source_index, ());
+        }
+
+        if let Some(source_index) = source_index(graph, functions, Some(&loopCounter.to)) {
+          graph.add_edge(index, source_index, ());
+        }
+
+        if let Some(source_index) = source_index(graph, functions, Some(&loopCounter.step)) {
+          graph.add_edge(index, source_index, ());
+        }
+
+        if let Some(data_ins) = data_ins {
+          for data_in in data_ins {
+            if let Some(source_index) = source_index(graph, functions, data_in.source.as_deref()) {
+              graph.add_edge(index, source_index, ());
+            }
+          }
+        }
+
+        for function in loopBody {
+          function.add_to_graph(graph, functions);
+        }
+
+        if let Some(data_outs) = data_outs {
+          for data_out in data_outs {
+            if let Some(source_index) = source_index(graph, functions, data_out.source.as_deref()) {
+              graph.add_edge(index, source_index, ());
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+impl FunctionChoreography {
+  pub fn to_graph(&self) {
+    let mut graph = Graph::<&str, ()>::new();
+
+    let mut functions = HashMap::<&str, NodeIndex>::new();
+
+    self.add_to_graph(&mut graph, &mut functions);
+
+    dbg!(&functions);
+    dbg!(&graph);
+
+
+    use petgraph::dot::{Dot, Config};
+
+    println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
+
+
+  }
+}
--- homework-07/scheduler/src/main.rs
+++ homework-07/scheduler/src/main.rs
@@ -1,17 +1,15 @@
 use std::fs::File;
-use std::collections::HashMap;
 
-use serde_yaml::Value;
-
-struct FunctionChoreography {
-  workflow_body: Value,
-}
+mod afcl;
+use afcl::FunctionChoreography;
 
 fn main() -> anyhow::Result<()> {
-  let mut file = File::open("../../project/stock-fc.yml")?;
+  let file = File::open("../../project/stock-fc.yml")?;
 
-  let fc: Value = serde_yaml::from_reader(&file)?;
-  dbg!(fc);
+  let fc: FunctionChoreography = serde_yaml::from_reader(&file)?;
+  dbg!(&fc);
+
+  fc.to_graph();
 
   Ok(())
 }
