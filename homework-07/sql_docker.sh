70dc9a3a052efa293e5ddc35806bbc0411b93bb1
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2021-01-19 23:02:52 +0100

    Persist measurements in database.

--- homework-07/sql_docker.sh
+++ homework-07/sql_docker.sh
@@ -8,10 +8,18 @@ fi
 
 ${engine}-compose up -d
 
-${engine} exec -i db bash <<-EOF
+${engine} exec -i db bash <<-'EOF'
   apt-get update
   apt-get install curl --yes
   curl -L -o afcldb.sql https://raw.githubusercontent.com/sashkoristov/DAppMaster-2020W/master/H07/AFCLDB.sql
+
+  cat <<-'AFCL' >> afcldb.sql
+INSERT INTO `functiontype` (`id`, `name`, `type`, `avgRTT`, `avgCost`) VALUES (13, 'fetchProcess', 'fetchProcessType', 3627, 0.0000000308295);
+INSERT INTO `functiontype` (`id`, `name`, `type`, `avgRTT`, `avgCost`) VALUES (14, 'forecast', 'forecastType', 2333, 0.0000000568565);
+INSERT INTO `functiontype` (`id`, `name`, `type`, `avgRTT`, `avgCost`) VALUES (15, 'processResult', 'processResultType', 2826, 0.000000024021);
+INSERT INTO `functiontype` (`id`, `name`, `type`, `avgRTT`, `avgCost`) VALUES (16, 'createChart', 'createChartType', 1396, 0.000000011866);
+AFCL
+
   mysql -u root -pmariadb -e "create database afcl"
   mysql -u root -pmariadb afcl < afcldb.sql
 EOF
